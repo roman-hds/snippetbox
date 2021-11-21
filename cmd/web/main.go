aac969f453105d232bc54160d463c9ce5d1b5ce6
roman-hds <roman.avdashkevich@hitachivantara.com>
2021-11-20 21:15:51 -0500

    Add render helper method

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"flag"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -11,12 +12,12 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-// Define an application struct to hold the application-wide dependencies for the
-// web application. Adding a snippets field will allow us to make SnippetModel object available to our handlers
+// Define an application struct to hold the application-wide dependencies
 type application struct {
-	errorLog *log.Logger
-	infoLog  *log.Logger
-	snippets *mysql.SnippetModel
+	errorLog      *log.Logger
+	infoLog       *log.Logger
+	snippets      *mysql.SnippetModel
+	templateCache map[string]*template.Template
 }
 
 func main() {
@@ -27,43 +28,40 @@ func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
-	// To keep the main() function tidy, we put the code for creating a connection pool
-	// into the separate openDB() function below. We pass openDB() the DSN from the command-line flag
 	db, err := openDB(*dsn)
 	if err != nil {
 		errorLog.Fatal(err)
 	}
-	// We also defer a call to db.Close(), so that the connection pool is closed before the main() function exits
+	// We defer a call to db.Close(), so that the connection pool is closed before the main() function exits
 	defer db.Close()
 
-	// Initialize a new instance of application containing the dependencies.
-	app := &application{
-		errorLog: errorLog,
-		infoLog:  infoLog,
-		snippets: &mysql.SnippetModel{DB: db},
+	templateCache, err := newTemplateCache("./ui/html")
+	if err != nil {
+		errorLog.Fatal(err)
 	}
 
-	// Initialize a new http.Server struct. We set the Addr and Handler fields so
-	// that the server uses the same network address and routes as before, and set
-	// the ErrorLog field so that the server now uses the custom errorLog logger in
-	// the event of any problems.
+	app := &application{
+		errorLog:      errorLog,
+		infoLog:       infoLog,
+		snippets:      &mysql.SnippetModel{DB: db},
+		templateCache: templateCache,
+	}
+
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
 		Handler:  app.routes(), // Call the new app.routes() method
 	}
 
-	// Write messages using the two new loggers, instead of the standard logger.
 	infoLog.Printf("Starting server on %s", *addr)
-	// Call the ListenAndServe() method on our new http.Server struct.
 	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
 
 // The openDB() function wraps sql.Open() and returns a sql.DB connection pool for a given DSN
-// The sql.Open() function doesn't create any connections, all it does is initialize the pool for future use.
+// The sql.Open() function doesn't create any connections, it initializes the pool for future use.
 // Actual connections to the database are established lazily, as and when needed for the first time.
-// So to verify that everything is set up correctly we need to use the db.Ping() method to create a connection and check for any errors.
+// To verify that everything is set up correctly use the db.Ping() method to create a connection and check for any errors.
 func openDB(dsn string) (*sql.DB, error) {
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,9 +1,58 @@
 package main
 
-import "roman-hds/snippetbox/pkg/models"
+import (
+	"html/template"
+	"path/filepath"
+	"roman-hds/snippetbox/pkg/models"
+)
 
 // templateData acts as a holding struct for any dynamic data that we want to pass to HTML templates
 type templateData struct {
 	Snippet  *models.Snippet
 	Snippets []*models.Snippet
+}
+
+func newTemplateCache(dir string) (map[string]*template.Template, error) {
+	// Initialize a new map to act as the cache
+	cache := map[string]*template.Template{}
+
+	// Use the filepath.Glob() to get a slice of all felipaths with the extention '.page.tmpl'.
+	// This essentially gives us a slice of all the 'page' templates for the application
+	pages, err := filepath.Glob(filepath.Join(dir, "*.page.tmpl"))
+	if err != nil {
+		return nil, err
+	}
+
+	// Loop trough the pages one-by-one
+	for _, page := range pages {
+		// Extract the file names (like 'home.page.tmpl') from the full file path
+		// and assign it to the name variable
+		name := filepath.Base(page)
+
+		// Parse the page template file into a template set
+		ts, err := template.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+		// Use the ParseGlob() to add any 'layout' templates to the template set
+		// in our case, it's just the 'base' layout' at the moment
+		ts, err = ts.ParseGlob(filepath.Join(dir, "*.layout.tmpl"))
+		if err != nil {
+			return nil, err
+		}
+
+		// Use the ParseGlob method to add any 'partial' templates to the
+		// template set (in our case, it's just the 'footer' partial at the moment).
+		ts, err = ts.ParseGlob(filepath.Join(dir, "*.partial.tmpl"))
+		if err != nil {
+			return nil, err
+		}
+
+		// Add the template set to the cache, using the name of the page
+		// (like 'home.page.tmpl') as the key
+		cache[name] = ts
+	}
+
+	// Return the map
+	return cache, nil
 }
