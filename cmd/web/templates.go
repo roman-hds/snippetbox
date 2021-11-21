ca7d923f588d6e7506fe66ecf6e525aa3de742a5
roman-hds <roman.avdashkevich@hitachivantara.com>
2021-11-20 22:10:23 -0500

    Add custom template functions

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -7,6 +7,15 @@ import (
 	"time"
 )
 
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+// Initialize a template.FuncMap object and store it in a global variable.
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
 // templateData acts as a holding struct for any dynamic data that we want to pass to HTML templates
 type templateData struct {
 	CurrentYear int
@@ -15,46 +24,37 @@ type templateData struct {
 }
 
 func newTemplateCache(dir string) (map[string]*template.Template, error) {
-	// Initialize a new map to act as the cache
 	cache := map[string]*template.Template{}
 
-	// Use the filepath.Glob() to get a slice of all felipaths with the extention '.page.tmpl'.
-	// This essentially gives us a slice of all the 'page' templates for the application
 	pages, err := filepath.Glob(filepath.Join(dir, "*.page.tmpl"))
 	if err != nil {
 		return nil, err
 	}
 
-	// Loop trough the pages one-by-one
 	for _, page := range pages {
-		// Extract the file names (like 'home.page.tmpl') from the full file path
-		// and assign it to the name variable
 		name := filepath.Base(page)
 
-		// Parse the page template file into a template set
-		ts, err := template.ParseFiles(page)
+		// The template.FuncMap must be registered with the template set before you
+		// call the ParseFiles() method. This means we have to use template.New() to
+		// create an empty template set, use the Funcs() method to register the
+		// template.FuncMap, and then parse the file as normal.
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
-		// Use the ParseGlob() to add any 'layout' templates to the template set
-		// in our case, it's just the 'base' layout' at the moment
+
 		ts, err = ts.ParseGlob(filepath.Join(dir, "*.layout.tmpl"))
 		if err != nil {
 			return nil, err
 		}
 
-		// Use the ParseGlob method to add any 'partial' templates to the
-		// template set (in our case, it's just the 'footer' partial at the moment).
 		ts, err = ts.ParseGlob(filepath.Join(dir, "*.partial.tmpl"))
 		if err != nil {
 			return nil, err
 		}
 
-		// Add the template set to the cache, using the name of the page
-		// (like 'home.page.tmpl') as the key
 		cache[name] = ts
 	}
 
-	// Return the map
 	return cache, nil
 }
